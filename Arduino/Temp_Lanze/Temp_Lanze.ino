b06656b25eca1a730be2e1501842fa13579a8e9b
Michael Fölsner <59766204+mikefoelsner@users.noreply.github.com>
2020-01-22 13:07:49 +0100

    Add files via upload
    Arduinoansteuerung - Temperaturauswertung Lanze DS18B20

---
+++ Arduino/Temp_Lanze/Temp_Lanze.ino
@@ -0,0 +1,18 @@
+//Libraries für den Sensor
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature temp_lanze(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  temp_lanze();
+}
+void loop() {
+  temp_lanze.requestTemperatures();
+  Serial.print("Temperatur an Lanze: ");
+  Serial.print("temp_lanze.getTempCByIndex(0));
+  Serial.println(" °C");
+}
