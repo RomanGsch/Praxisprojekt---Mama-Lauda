25f56b97d20515d811bad6ec95a593ca26d04738
elnoscho <1910809130@stud.fh-kufstein.ac.at>
2020-01-24 18:25:51 +0100

    gui weiter

--- Raspberry/gui.py
+++ Raspberry/gui.py
@@ -1,16 +1,33 @@
 import tkinter as tk
 
+
+def start():
+    print('Start...')
+
+
+def stop():
+    print('Stop...')
+
+
 def main():
     mainWindow = tk.Tk(className="Mamma Lauda")
     w = 320#1280  # width for the Tk root
     h = 720#720  # height for the Tk root
     x = 0
     y = 0
+    Temp = 100
     mainWindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    labelUeberschrift = tk.Label(master=mainWindow, text="Team --- MAMMA LAUDA")
+    buttonStart = tk.Button(master=mainWindow, text="Starten", command=start)
+    buttonStop = tk.Button(master=mainWindow, text="Stop", command=stop)
+    labelTemp = tk.Label(master=mainWindow, text=Temp)
 
-    #mainWindow.geometry("500x500")
-    #mainWindow.resizable(width=None, height=None)
-    #mainWindow.
+
+    labelUeberschrift.pack()
+    labelTemp.pack()
+    buttonStart.pack()
+    buttonStop.pack()
+
     mainWindow.mainloop()
 
 if __name__ == '__main__':
