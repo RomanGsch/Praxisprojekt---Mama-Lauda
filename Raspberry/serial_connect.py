f6c97c2b79b97ffc048f8d9cfdc444bd92e43e3a
elnoscho <1910809130@stud.fh-kufstein.ac.at>
2020-02-01 11:21:23 +0000

    serial dazu gegeben, tcp getestet passt zumindest mit nachrichten hin und her schicken

---
+++ Raspberry/serial_connect.py
@@ -0,0 +1,13 @@
+import serial
+from time import sleep
+
+ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate=9600
+
+while True:
+    read_ser=ser.readline()
+    write_ser = ser.write("fahren")
+    print(read_ser)
+    if(read_ser=="Hello From Arduino!"):
+        print("bekommen")
+    sleep(1)
