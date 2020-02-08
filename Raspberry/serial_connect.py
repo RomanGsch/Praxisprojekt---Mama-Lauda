4f3ebd1bea99456fadbd63f1d5f1231fbb297c8f
elnoscho <1910809130@stud.fh-kufstein.ac.at>
2020-02-08 09:45:51 +0000

    nothing happened

--- Raspberry/serial_connect.py
+++ Raspberry/serial_connect.py
@@ -1,15 +1,16 @@
 import serial
 from time import sleep
 
-ser = serial.Serial("/dev/ttyACM0", 9600)  # change ACM number as found from ls /dev/tty/ACM*
+ser = serial.Serial("/dev/ttyACM1", 9600)  # change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate = 9600
 
 while True:
-    read_ser = ser.readline()
-    # write_ser = ser.write("fahren")
-    print(read_ser)
-    if read_ser == "Hello From Arduino!":
-        print("bekommen")
-    else:
-        print("not")
+    #read_ser = ser.readline()
+    text = "hallo"
+    write_ser = ser.write(text.encode())
+    #print(read_ser)
+    #if read_ser == "Hello From Arduino!":
+    #    print("bekommen")
+    #else:
+    #    print("not")
     sleep(1)
